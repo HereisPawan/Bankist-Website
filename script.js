2e929786112d049705b7a20498ada635ae8b11bb
Pawan Singh Bora (Capgemini.com) <pawan.singh-bora@capgemini.com>
2024-03-18 12:58:13 +0530

    Scroll to the required element feature and cookies div enabled

--- script.js
+++ script.js
@@ -7,8 +7,25 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const footer = document.querySelector('.footer');
+const cookies = document.createElement('div');
 
-const openModal = function () {
+//Create a cookies banner with dataset attributes.
+cookies.innerHTML =
+    'We use cookied for improved functionality and analytics . <button class="btn btn--close-cookie">Got it!</button>';
+cookies.classList.add('cookie-message');
+footer.append(cookies);
+cookies.style.height = '60px';
+document
+    .querySelector('.btn--close-cookie')
+    .addEventListener('click', function () {
+        cookies.remove();
+    });
+cookies.setAttribute('data-id', 'cookies');
+
+//Prevent default behaviour to move the page at the top when clicked on open account .
+const openModal = function (e) {
+    e.preventDefault();
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 };
@@ -18,8 +35,9 @@ const closeModal = function () {
     overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-    btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach((value) => {
+    value.addEventListener('click', openModal);
+});
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -28,4 +46,11 @@ document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
     }
+});
+
+//To scroll at the element section--1 when clicked on learn more
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+btnScrollTo.addEventListener('click', function () {
+    section1.scrollIntoView({ behavior: 'smooth' });
 });
